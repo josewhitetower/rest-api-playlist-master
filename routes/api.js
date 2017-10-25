eb5d3091ab62ccadbb7a4a1115724845303d6ed1
Jose Torreblanca <josewhitetower@gmail.com>
2017-10-25 18:16:00 +0200

    PUT and DELETE

--- routes/api.js
+++ routes/api.js
@@ -3,26 +3,34 @@ const router = express.Router();
 const Ninja = require("../models/ninja");
 
 // get the list of ninjas from the database
-router.get("/ninjas", function(req, res) {
+router.get("/ninjas", function(req, res, next) {
   res.send({ type: "GET" });
 });
 
 //add a new ninja to the database
-router.post("/ninjas", (req, res) => {
-  Ninja.create(req.body).then(ninja => {
-    res.send(ninja);
-  });
+router.post("/ninjas", (req, res, next) => {
+  Ninja.create(req.body)
+    .then(ninja => {
+      res.send(ninja);
+    })
+    .catch(next);
 });
 // var ninja = new Ninja(req.body);
 // ninja.save();
 
 //update a ninja in the database
-router.put("/ninjas/:id", function(req, res) {
-  res.send({ type: "PUT" });
+router.put("/ninjas/:id", function(req, res, next) {
+  Ninja.findByIdAndUpdate({ _id: req.params.id }, req.body).then(() => {
+    Ninja.findOne({ _id: req.params.id }).then(ninja => {
+      res.send(ninja);
+    });
+  });
 });
 //delete a ninja from the database
-router.delete("/ninjas/:id", function(req, res) {
-  res.send({ type: "DELETE" });
+router.delete("/ninjas/:id", function(req, res, next) {
+  Ninja.findByIdAndRemove({ _id: req.params.id }).then(ninja => {
+    res.send(ninja);
+  });
 });
 
 module.exports = router;
